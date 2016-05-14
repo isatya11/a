f540183b35d7c68bb7ee9c2a09e3e2624fd79f8b
YS Satya <satya11196@gmail.com>
2016-05-14 14:30:38 +0530

    trial file

---
+++ trial.java
@@ -0,0 +1,50 @@
+import java.util.*;
+public class trial
+{
+   static Iterator func(ArrayList mylist)
+   {
+        int k=0;
+      Iterator it=mylist.iterator();
+      String s;
+      while(it.hasNext())
+      {
+         Object element = it.next();
+         s=element+"";
+         //System.out.println(s);
+        if(s.compareTo("###")==0)//Hints: use instanceof operator
+           break;
+        k++;
+      }
+      System.out.println("k="+k);
+      return it;
+      
+   }
+
+   public static void main(String []argh)
+   {
+        int k=0;
+      ArrayList mylist = new ArrayList();
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      int m=sc.nextInt();
+      for(int i=0;i<n;i++)
+      {
+         mylist.add(sc.nextInt());
+      }
+      mylist.add("###");
+      for(int i=0;i<m;i++)
+      {
+         mylist.add(sc.next());
+      }
+      
+      
+      Iterator it=func(mylist);
+      while(it.hasNext())
+      {
+         Object element = it.next();
+         System.out.println((String)element);
+         k++;
+      }
+      System.out.println("k="+k);
+   }
+}
